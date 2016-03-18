767cb927eb1642813b7fc5a43c5c53380ba6872e
Roman Kozvonin <romankozvonin@gmail.com>
2016-03-18 09:20:58 +0400

    17-03-2016 Update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,8 +19,10 @@
 var ready = function(){
                         $(document).foundation();
                         //checkTopElementPosition();
-                        //setTimeout(function(){Foundation.reInit('sticky');}, 450);
-                        
+                        footerControl();
+                        $('img').load(function(){footerControl();});
+                        $(window).resize(function(){footerControl();});
+                       // setTimeout(function(){footerControl();}, 450);
                       };
 
 $(document).ready(ready);
@@ -34,7 +36,24 @@ function checkTopElementPosition()
         $("#top").css('top', $('.top-bar').outerHeight(true) + 'px');
     }
     //if ($("#top").offset().top > $('.top-bar').height() + $('.top-bar').height()/2) { $("#top").css('top', '0px'}
-    
-    
+}
+function footerControl()
+{
+    var tbHeight = $("#top_bar").outerHeight(true);
+    var tHeight = $("#top").outerHeight(true);
+    var wHeight = $("#wrapper").outerHeight(true);
+    var fHeight = $("#footer").outerHeight(true);
+    var windHeight = $(window).height();
+    var sumHeight = tbHeight + tHeight + wHeight + fHeight;
+    if (sumHeight > windHeight)
+    {
+        $("#footer").addClass('rel-footer');
+        $("#footer").removeClass('fix-footer');
+    }else
+    {
+        $("#footer").addClass('fix-footer');
+        $("#footer").removeClass('rel-footer');
+    }
+    console.log(sumHeight + ' ' + windHeight)
 }
 
